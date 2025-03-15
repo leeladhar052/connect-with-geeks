aeef9d7c2c8ac4c2696c8865835f1f094619887f
Leeladhar <139422865+leeladhar052@users.noreply.github.com>
2025-03-16 00:39:12 +0530

    commit

--- codesync-frontend/src/pages/Settings.jsx
+++ codesync-frontend/src/pages/Settings.jsx
@@ -1,15 +1,15 @@
-import ProfileSettings from "../components/settings/ProfileSettings";
-import ThemeSwitcher from "../components/settings/ThemeSwitcher";
-import Notifications from "../components/settings/Notifications";
+// import ProfileSettings from "../components/settings/ProfileSettings";
+// import ThemeSwitcher from "../components/settings/ThemeSwitcher";
+// import Notifications from "../components/settings/Notifications";
 
-const Settings = () => {
-  return (
-    <div className="p-6">
-      <ProfileSettings />
-      <ThemeSwitcher />
-      <Notifications />
-    </div>
-  );
-};
+// const Settings = () => {
+//   return (
+//     <div className="p-6">
+//       <ProfileSettings />
+//       <ThemeSwitcher />
+//       <Notifications />
+//     </div>
+//   );
+// };
 
-export default Settings;
+// export default Settings;
